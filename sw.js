315fd64c76eb4bb2bd483048781a260b910f21e5
badoza <dodge.kunal@gmail.com>
2026-04-14 19:48:05 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,37 @@
-const CACHE_NAME = 'inspire-v2';
+const CACHE_NAME = 'inspire-v4';
+
+// HARDCODED to your exact GitHub Pages URL
 const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
+  '/inspire.in/',
+  '/inspire.in/index.html',
+  '/inspire.in/manifest.json',
+  '/inspire.in/logo.png',
+  '/inspire.in/intro.mp4'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting(); // Forces the new version to install immediately
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Deletes the old broken app cache
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
