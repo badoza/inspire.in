a412bc7adaf69c7867f5d5a5095cbe2404aeb60a
badoza <dodge.kunal@gmail.com>
2026-04-14 18:32:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,16 @@
-const CACHE_NAME = 'inspire-v1';
+const CACHE_NAME = 'inspire-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
